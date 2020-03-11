d059baccb61cc40256fdc0c215f7f49b70c4af00
Duncan Smith <duncan@dubsado.com>
2020-03-10 21:13:50 -0700

    Roundtrip test finished and passing

--- xwt.go
+++ xwt.go
@@ -43,19 +43,31 @@ type SerializedToken = []byte
 
 // Token represents a token
 type Token struct {
-	Data      []byte
 	Signature []byte
+	Data      []byte
 	Version   int
 	Expires   int64
 	ID        []byte
 }
 
+// NewTokenV1 returns a Token with the given values. If a KeyPair is given, the returned Token will be signed.
+func NewTokenV1(expires int64, id []byte, kp *KeyPair) (*Token, error) {
+	t := Token{Version: 1, Expires: expires, ID: id}
+	t.serializeData()
+	if kp != nil {
+		if err := t.Sign(kp); err != nil {
+			return nil, err
+		}
+	}
+	return &t, nil
+}
+
 // Parse extracts the information from a SerializedToken
 func Parse(bz SerializedToken) (*Token, error) {
-	signatureBz := bz[:64]
-	versionBz := bz[64:66]
-	expiresBz := bz[66:74]
-	idBz := bz[74:]
+	signatureBz := bz[:signatureLength]
+	versionBz := bz[signatureLength:(signatureLength + versionLength)]
+	expiresBz := bz[(signatureLength + versionLength):(signatureLength + versionLength + expiresLength)]
+	idBz := bz[(signatureLength + versionLength + expiresLength):]
 
 	if string(versionBz) != "v1" {
 		return nil, fmt.Errorf("Invalid token version: %v", string(bz))
@@ -65,7 +77,7 @@ func Parse(bz SerializedToken) (*Token, error) {
 		return nil, fmt.Errorf("Error parsing expiry: %v %v", string(bz), err)
 	}
 	t := Token{
-		Data:      bz,
+		Data:      bz[signatureLength:],
 		Signature: signatureBz,
 		Version:   1,
 		Expires:   expires,
@@ -74,12 +86,45 @@ func Parse(bz SerializedToken) (*Token, error) {
 	return &t, nil
 }
 
+// Serialize encodes a Token as bytes in the format (signature | version | expires | id)
+func (t *Token) Serialize() SerializedToken {
+	bz := make([]byte, signatureLength+versionLength+expiresLength+len(t.ID))
+	copy(bz[:signatureLength], t.Signature)
+	bz = append(bz, t.serializeData()...)
+	return bz
+}
+
+// serializeData encodes the non-signature data of a Token as bytes in the format (version | expires | id)
+func (t *Token) serializeData() []byte {
+	if len(t.Data) > 0 {
+		return t.Data
+	}
+	bz := make([]byte, versionLength+expiresLength+len(t.ID))
+	copy(bz[:versionLength], []byte(fmt.Sprintf("v%v", t.Version)))
+	expiresBz := []byte(strconv.FormatInt(t.Expires, 10))
+	copy(bz[versionLength:(versionLength+expiresLength)], expiresBz)
+	copy(bz[(versionLength+expiresLength):], t.ID)
+	t.Data = bz
+	return bz
+}
+
+// Sign signs the non-signature data and sets the signature
+func (t *Token) Sign(kp *KeyPair) error {
+	if len(t.Data) == 0 {
+		t.serializeData()
+	}
+	signed := sign.Sign(nil, t.Data, kp.PrivateKey)
+	t.Signature = signed[:signatureLength]
+	return nil
+}
+
 // Verify returns an error if a token is not valid
-func (t Token) Verify(pubkey [32]byte, expiryGracePeriod time.Duration) error {
-	signedBz := make([]byte, len(t.Data)+signatureLength)
-	copy(signedBz[:signatureLength], t.Signature[:])
-	copy(signedBz[signatureLength:], t.Data[:])
-	_, valid := sign.Open(nil, signedBz, &pubkey)
+func (t *Token) Verify(pubkey [32]byte, expiryGracePeriod time.Duration) error {
+	sigWithMessage := make([]byte, len(t.Signature)+len(t.Data))
+	copy(sigWithMessage[:signatureLength], t.Signature)
+	copy(sigWithMessage[signatureLength:], t.Data)
+
+	_, valid := sign.Open(nil, sigWithMessage, &pubkey)
 	if !valid {
 		return errors.New("Invalid signature")
 	}
@@ -87,33 +132,6 @@ func (t Token) Verify(pubkey [32]byte, expiryGracePeriod time.Duration) error {
 	if time.Now().After(finalExpiry) {
 		return errors.New("Expired token")
 	}
-	return nil
-}
-
-// Serialize encodes a Token as bytes in the format (signature | version | expires | id)
-func (t Token) Serialize() []byte {
-	bz := make([]byte, signatureLength+versionLength+expiresLength+len(t.ID))
-	copy(bz[:64], t.Signature)
-	bz = append(bz, t.SerializeData()...)
-	return bz
-}
-
-// SerializeData encodes the non-signature data of a Token as bytes in the format (version | expires | id)
-func (t Token) SerializeData() []byte {
-	bz := make([]byte, versionLength+expiresLength+len(t.ID))
-	copy(bz[0:2], []byte(fmt.Sprintf("v%v", t.Version)))
-	expiresBz := []byte(strconv.FormatInt(t.Expires, 10))
-	copy(bz[2:12], expiresBz)
-	copy(bz[12:], t.ID)
-	return bz
-}
-
-// Sign signs the non-signature data and sets the signature
-func (t Token) Sign(kp *KeyPair) error {
-	signed := sign.Sign(nil, t.SerializeData(), kp.PrivateKey)
-	var signature [64]byte
-	copy(signature[:], signed[:64])
-	t.Signature = signature[:]
 	return nil
 }
 
--- xwt_test.go
+++ xwt_test.go
@@ -2,8 +2,8 @@ package xwt
 
 import (
 	"bytes"
-	"fmt"
 	"testing"
+	"time"
 )
 
 func TestRoundtrip(t *testing.T) {
@@ -12,16 +12,14 @@ func TestRoundtrip(t *testing.T) {
 		t.Fatalf("Error generating keys: %v", err)
 	}
 
-	token := Token{
-		ID:      []byte("user:12345"),
-		Expires: 1583826163,
-		Version: 1,
-	}
+	token, err := NewTokenV1(1583826163, []byte("user:12345"), nil)
 
-	if err = token.Sign(kp); err != nil {
+	if err := token.Sign(kp); err != nil {
 		t.Fatalf("Error signing token: %v", err)
 	}
 
-	fmt.Println("Keys:", kp.PublicHex())
-	panic("ono")
+	sinceEpoch := time.Now().Sub(time.Unix(1583826163, 0))
+	if err = token.Verify(*kp.PublicKey, sinceEpoch+time.Second); err != nil {
+		t.Fatalf("Error verifying token: %v", err)
+	}
 }
